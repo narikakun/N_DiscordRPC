46d85c4d33b633ee3b4ace9d0c58121d97a41175
なりかくん <narikakun@gmail.com>
2020-08-22 16:54:23 +0900

    作りかけ、めんどくなった

--- N_DiscordRPC/Form1.Designer.cs
+++ N_DiscordRPC/Form1.Designer.cs
@@ -28,13 +28,108 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.clientid_textbox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.details_textbox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.state_textbox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Client ID";
+            // 
+            // clientid_textbox
+            // 
+            this.clientid_textbox.Font = new System.Drawing.Font("MS UI Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.clientid_textbox.Location = new System.Drawing.Point(98, 6);
+            this.clientid_textbox.Name = "clientid_textbox";
+            this.clientid_textbox.Size = new System.Drawing.Size(282, 26);
+            this.clientid_textbox.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(45, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 19);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "詳細";
+            // 
+            // details_textbox
+            // 
+            this.details_textbox.Font = new System.Drawing.Font("MS UI Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.details_textbox.Location = new System.Drawing.Point(98, 38);
+            this.details_textbox.Name = "details_textbox";
+            this.details_textbox.Size = new System.Drawing.Size(282, 26);
+            this.details_textbox.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Location = new System.Drawing.Point(45, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 19);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "状態";
+            // 
+            // state_textbox
+            // 
+            this.state_textbox.Font = new System.Drawing.Font("MS UI Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.state_textbox.Location = new System.Drawing.Point(98, 70);
+            this.state_textbox.Name = "state_textbox";
+            this.state_textbox.Size = new System.Drawing.Size(282, 26);
+            this.state_textbox.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button1.Location = new System.Drawing.Point(305, 102);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 30);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "更新";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(386, 138);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.state_textbox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.details_textbox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.clientid_textbox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Discordステータス変更ソフト";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox clientid_textbox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox details_textbox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox state_textbox;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- N_DiscordRPC/Form1.cs
+++ N_DiscordRPC/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DiscordRPC;
+using DiscordRPC.Logging;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,5 +18,30 @@ namespace N_DiscordRPC
         {
             InitializeComponent();
         }
+
+		public DiscordRpcClient client;
+
+        void Initialize()
+		{
+			var clientID = "746622619054243891";
+			if (clientid_textbox.Text != "")
+            {
+				clientID = clientid_textbox.Text;
+            }
+			client = new DiscordRpcClient(clientID);
+			client.Logger = new ConsoleLogger() { Level = LogLevel.Warning };
+
+			client.Initialize();
+
+			client.SetPresence(new RichPresence()
+			{
+				Details = details_textbox.Text,
+				State = state_textbox.Text,
+			});
+		}
+        private void button1_Click(object sender, EventArgs e)
+        {
+			Initialize();
+		}
     }
 }
